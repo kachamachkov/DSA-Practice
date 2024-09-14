1ea29decb1232a1dce3d4f07eb6477588694614a
kachamachkov <kachamachkovboyan@gmail.com>
2024-09-14 15:59:06 +0300

    Add pop mechanism

--- Stack/theory/stack.js
+++ Stack/theory/stack.js
@@ -9,7 +9,17 @@ class Stack {
     this.items[this.count] = element;
     console.log(`${element} added to ${this.count}`);
     this.count++;
-    return this.count - 1;
+    return this.count - 1; //return position of addition
+  }
+
+  // return and remove top element in stack
+  // return undefined if stack is empty
+  pop() {
+    if (this.count == 0) return undefined;
+    let deleteItem = this.items[this.count - 1];
+    this.count--;
+    console.log(`${deleteItem} removed`);
+    return deleteItem;
   }
 }
 
@@ -18,3 +28,6 @@ const stack = new Stack();
 stack.push(100);
 stack.push(200);
 stack.push(300);
+
+stack.pop();
+stack.pop();
